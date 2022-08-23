02eb0fc7e0164f86bf8128f0587d03e1cf26447d
Barry Yap <barry_yap@aiap.sg>
2022-08-23 18:51:43 +0800

    end of day 13 - added code for displaying event details

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
+import formatTimestamp from "../../utils/formatTimestamp";
+import Image from "next/image";
 
 import {
   EmojiHappyIcon,
@@ -7,16 +11,17 @@ import {
   LinkIcon
 } from "@heroicons/react/outline";
 
-function Event() {
+function Event({event}) {
+  console.log("EVENT:", event);
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
-        <title>name | web3rsvp</title>
-        <meta name="description" content={"name"} />
+        <title> {event.name} | web3rsvp</title>
+        <meta name={event.description} content={event.name} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="relative py-12">
-        <h6 className="mb-2">time</h6>
+        <h6 className="mb-2">{formatTimestamp(event.eventTimestamp)}</h6>
         <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl mb-6 lg:mb-12">
           name
         </h1>
@@ -25,14 +30,20 @@ function Event() {
             <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
               
             </div>
-            <p>description</p>
+            <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+              {event.imageURL && (
+                <Image src={event.imageURL} alt="event image" layout="fill" />
+              )}
+            </div>
+
+            <p>{event.description}</p>
           </div>
           <div className="max-w-xs w-full flex flex-col gap-4 mb-6 lg:mb-0">
            
             <div className="flex item-center">
               <UsersIcon className="w-6 mr-2" />
               <span className="truncate">
-                # attending
+                {event.totalRSVPs}/{event.maxCapacity} attending
               </span>
             </div>
             <div className="flex item-center">
@@ -45,7 +56,7 @@ function Event() {
                 Hosted by{" "}
                 <a
                   className="text-indigo-800 truncate hover:underline"
-                  href={""}
+                  href={{`${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}address/${event.eventOwner}`}
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -58,6 +69,46 @@ function Event() {
       </section>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+
+  const { data } = await client.query({
+    query: gql`
+      query Event($id: String!) {
+        event(id: $id) {
+          id
+          eventID
+          name
+          description
+          link
+          eventOwner
+          eventTimestamp
+          maxCapacity
+          deposit
+          totalRSVPs
+          totalConfirmedAttendees
+          imageURL
+          rsvps {
+            id
+            attendee {
+              id
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id: id,
+    },
+  });
+
+  return {
+    props: {
+      event: data.event,
+    },
+  };
 }
 
 export default Event;
